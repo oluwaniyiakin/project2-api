cb814b24641aa2b9d8fb5ffc7342fc7fa39326d5
OLUWANIYIAKIN <olu24002@byui.edu>
2025-03-28 14:59:44 +0100

    ss

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const passport = require("passport");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 require("./config/passport"); // Import Passport config
 
 dotenv.config();
 const app = express();
 
 // Middleware
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: true, credentials: true }));
@@ -19,6 +24,7 @@ app.use(passport.initialize());
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/items", require("./routes/items")); // ✅ Corrected to "items"
 
 // Connect to MongoDB
 mongoose
